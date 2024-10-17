ecfe695435e40061460ad718bf13c63467b4773e
vasanth10101 <166881936+vasanth10101@users.noreply.github.com>
2024-10-17 14:10:06 +0530

    Update File_system.sh

--- File_system.sh
+++ File_system.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+yellow="\e[1;33m"
+normal="\e[0m"
+echo -e "${yellow}checking file permission${normal}"
 # Declare the file_system as an array with individual elements
 file_system=("/home" "/tmp" "/var/tmp" "var/lib" "var/log")
 
