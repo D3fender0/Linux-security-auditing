e19d2810105feae346395be23ad7b7a045257c48
vasanth10101 <166881936+vasanth10101@users.noreply.github.com>
2024-10-19 10:35:41 +0530

    Update User_account.sh

--- User_account.sh
+++ User_account.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-   
+# Define colors
 yellow="\e[1;33m"
 normal="\e[0m"
 
-echo -e "${yellow} User account ${normal}"
+# Print header
+echo -e "${yellow}User account${normal}"
 
 # List all users from /etc/passwd
-user_account = $(cut -d: -f1 /etc/passwd | sort)
+user_account=$(cut -d: -f1 /etc/passwd | sort)
 echo "${user_account}"
 
 # List all users who logged in from 'last' command
 echo "Recently used"
-recently_used=$(last | awk '{print $1}' | sort | uniq | head -n 5) 
+recently_used=$(last | awk '{print $1}' | sort | uniq | head -n 5)
 echo "${recently_used}"
-
-
